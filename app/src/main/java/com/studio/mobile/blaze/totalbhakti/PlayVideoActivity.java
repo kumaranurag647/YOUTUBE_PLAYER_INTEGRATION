60dfaf85bbddf1c4da2c317c30961d006f2d5bd6
ANURAG KUMAR <kumaranurag647@gmail.com>
2018-10-21 02:05:34 +0530

    Add after Youtube Video issue Resolved

--- app/src/main/java/com/studio/mobile/blaze/totalbhakti/PlayVideoActivity.java
+++ app/src/main/java/com/studio/mobile/blaze/totalbhakti/PlayVideoActivity.java
@@ -27,10 +27,44 @@ public class PlayVideoActivity extends YouTubeFailureRecoveryActivity{
         super.onCreate(savedInstanceState);
         videoId = getIntent().getStringExtra("videoId");
         setContentView(R.layout.fullscreen_demo);
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
+        mInterstitialAd.setAdListener(new AdListener() {
+
+            @Override
+            public void onAdLoaded() {
+
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when the ad is displayed.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when when the interstitial ad is closed.
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+        });
+
         playerView = findViewById(R.id.player);
         playerView.initialize(DeveloperKey.DEVELOPER_KEY, this);
 
     }
+
 
     @Override
     public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player,
@@ -54,13 +88,9 @@ public class PlayVideoActivity extends YouTubeFailureRecoveryActivity{
     }
 
 
-
-
-
-
-
-
-
-
-
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mInterstitialAd.show();
+    }
 }
